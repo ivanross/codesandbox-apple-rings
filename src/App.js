a4f5caefc0796b37c72c1453355271773f8a2548
Ivan Rossi <ivanross.me@gmail.com>
2021-11-28 23:17:49 +0100

    Fix gradient rotation + add apple watch background

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { interpolateRgb } from "d3";
+import { range, scaleLinear } from "d3";
 import { useRef, useState } from "react";
 import "./styles.css";
 
@@ -9,6 +9,15 @@ function useId() {
 function partitionOnce(total, part) {
   if (total >= part) return [part, total - part];
   else return [total, 0];
+}
+
+/**
+ * @param {string[]} colors
+ */
+function interpolateColor(colors) {
+  const length = colors.length;
+  const domain = range(length).map((i) => i / (length - 1));
+  return scaleLinear().domain(domain).range(colors);
 }
 
 /**
@@ -24,11 +33,27 @@ function cumulate(values) {
   return values.map((value) => ({ value, from, to: (from += value) }));
 }
 
+/**
+ * Computes properties x1, y1, x2 and y2 of linearGradient
+ * to render a linear gradient with `angle`
+ *
+ * @param {number} angle
+ */
+function computeGradientRotation(angle) {
+  return {
+    x1: Math.cos(angle) * 0.5 + 0.5,
+    y1: Math.sin(angle) * 0.5 + 0.5,
+    x2: Math.cos(angle - Math.PI) * 0.5 + 0.5,
+    y2: Math.sin(angle - Math.PI) * 0.5 + 0.5
+  };
+}
+
 export default function App() {
-  const [progress, setProgress] = useState(0.5);
+  const [progress, setProgress] = useState(1.05);
   return (
     <div className="App">
-      <div>
+      <AppleWatch progress={progress} />
+      <div className="layer" style={{ alignItems: "start", padding: 30 }}>
         <input
           type="range"
           value={progress}
@@ -38,55 +63,68 @@ export default function App() {
           onChange={(e) => setProgress(+e.target.value)}
         />
       </div>
-      <SVG progress={progress} />
     </div>
   );
 }
 
-const SVG = ({ progress }) => {
+const AppleWatch = ({ progress }) => {
   const width = 400;
-
   const height = 400;
 
   const angle = progress * Math.PI * 2;
   const innerRadius = 70;
   const ringSpacing = 2;
-  const strokeWidth = 30;
+  const strokeWidth = 35;
 
   return (
-    <svg
-      width={width}
-      height={height}
-      viewBox={`0 0 ${width} ${height}`}
-      style={{ overflow: "hidden" }}
-    >
-      <rect width="100%" height="100%" fill="black" />
-
-      <AppleRing
-        colors={["#e63754", "#e64c85"]}
-        cx={width / 2}
-        cy={height / 2}
-        angle={angle}
-        radius={innerRadius + (strokeWidth + ringSpacing) * 2}
-        strokeWidth={strokeWidth}
-      />
-      <AppleRing
-        colors={["#e0fc52", "#b2fb4f"]}
-        cx={width / 2}
-        cy={height / 2}
-        angle={angle}
-        radius={innerRadius + strokeWidth + ringSpacing}
-        strokeWidth={strokeWidth}
-      />
-      <AppleRing
-        colors={["#75fbb0", "#60d6fa"]}
-        cx={width / 2}
-        cy={height / 2}
-        angle={angle}
-        radius={innerRadius}
-        strokeWidth={strokeWidth}
-      />
-    </svg>
+    <div style={{ position: "relative", width: "100vw", height: "100vh" }}>
+      <div class="layer">
+        <img
+          alt="watch"
+          src="https://mockuphone.com/static/images/devices/apple-applewatch44mm-darktitanium-cyprusgreensololoop-portrait.png"
+          style={{
+            width: width + 450,
+            height: height + 450,
+            objectFit: "scale-down"
+          }}
+        />
+      </div>
+      <div className="layer">
+        <div style={{ width, height }}>
+          <svg
+            width={width}
+            height={height}
+            viewBox={`0 0 ${width} ${height}`}
+            style={{ overflow: "hidden" }}
+          >
+            <AppleRing
+              colors={["#e63754", "#e64c85"]}
+              cx={width / 2}
+              cy={height / 2}
+              angle={angle}
+              radius={innerRadius + (strokeWidth + ringSpacing) * 2}
+              strokeWidth={strokeWidth}
+            />
+            <AppleRing
+              colors={["#b2fb4f", "#e0fc52"]}
+              cx={width / 2}
+              cy={height / 2}
+              angle={angle * 0.8}
+              radius={innerRadius + strokeWidth + ringSpacing}
+              strokeWidth={strokeWidth}
+            />
+            <AppleRing
+              colors={["#60d6fa", "#75fbb0"]}
+              cx={width / 2}
+              cy={height / 2}
+              angle={angle * 0.9}
+              radius={innerRadius}
+              strokeWidth={strokeWidth}
+            />
+          </svg>
+        </div>
+      </div>
+    </div>
   );
 };
 
@@ -101,15 +139,14 @@ const AppleRing = ({
 }) => {
   const partitionStep = Math.PI;
 
-  const ringAngle = Math.min(angle, Math.PI * 2);
-  const rotationAngle = angle - ringAngle;
-  const phase = initialPhase + rotationAngle;
+  const singleLapAngle = Math.min(angle, Math.PI * 2);
+  const ringRotationAngle = angle - singleLapAngle;
+  const phase = initialPhase + ringRotationAngle;
 
-  const partitionAngles = partitionOnce(ringAngle, partitionStep).reverse();
-  const segments = cumulate(partitionAngles);
-  const [segmentStart, segmentEnd] = segments;
+  const partitionAngles = partitionOnce(singleLapAngle, partitionStep);
+  const [segmentStart, segmentEnd] = cumulate(partitionAngles);
 
-  const colorInterp = interpolateRgb(...colors);
+  const colorInterp = interpolateColor(colors.slice(0, 3));
 
   const arcX = (angle) => Math.cos(angle + phase) * radius + cx;
   const arcY = (angle) => Math.sin(angle + phase) * radius + cy;
@@ -127,49 +164,29 @@ const AppleRing = ({
   const maskStartId = useId();
   const gradientShadowId = useId();
 
-  const p1 = {
-    x: Math.cos(ringAngle + phase) * 0.5 + 0.5,
-    y: Math.sin(ringAngle + phase) * 0.5 + 0.5
-  };
-
-  const p2 = {
-    x: Math.cos(ringAngle + phase - Math.PI) * 0.5 + 0.5,
-    y: Math.sin(ringAngle + phase - Math.PI) * 0.5 + 0.5
-  };
-
+  const gradientAngle = Math.max(0, angle - Math.PI * 2) + initialPhase;
+  const gradientRotation = computeGradientRotation(gradientAngle);
   const gradientSide = radius * 2 + strokeWidth;
   return (
     <g>
       <defs>
-        <linearGradient
-          id={gradientEndId}
-          x1={p1.x}
-          y1={p1.y}
-          x2={p2.x}
-          y2={p2.y}
-        >
-          <stop offset="0%" stopColor={colorInterp(0)} />
-          <stop offset="100%" stopColor={colorInterp(0.5)} />
+        <linearGradient id={gradientStartId} {...gradientRotation}>
+          <stop offset="15%" stopColor={colorInterp(0)} />
+          <stop offset="85%" stopColor={colorInterp(0.5)} />
         </linearGradient>
 
-        <linearGradient
-          id={gradientStartId}
-          x1={p1.x}
-          y1={p1.y}
-          x2={p2.x}
-          y2={p2.y}
-        >
-          <stop offset="0%" stopColor={colorInterp(1)} />
-          <stop offset="100%" stopColor={colorInterp(0.5)} />
+        <linearGradient id={gradientEndId} {...gradientRotation}>
+          <stop offset="15%" stopColor={colorInterp(1)} />
+          <stop offset="85%" stopColor={colorInterp(0.5)} />
         </linearGradient>
 
         <radialGradient id={gradientShadowId}>
-          <stop offset="30%" stopColor="black" />
+          <stop offset="40%" stopColor="black" />
           <stop offset="100%" stopColor="black" stopOpacity={0} />
         </radialGradient>
       </defs>
 
-      <mask id={maskEndId}>
+      <mask id={maskStartId}>
         <path
           d={path(segmentStart)}
           strokeWidth={strokeWidth}
@@ -179,7 +196,7 @@ const AppleRing = ({
         />
       </mask>
 
-      <mask id={maskStartId}>
+      <mask id={maskEndId}>
         <path
           d={path(segmentEnd)}
           strokeWidth={strokeWidth}
@@ -189,21 +206,14 @@ const AppleRing = ({
         />
       </mask>
 
-      <rect
-        x={cx - gradientSide / 2}
-        y={cy - gradientSide / 2}
-        width={gradientSide}
-        height={gradientSide}
-        fill={`url(#${gradientEndId})`}
-        mask={`url(#${maskEndId})`}
-      />
-
       <circle
-        r={strokeWidth / 2 + 6}
-        cx={arcX(segmentEnd.to)}
-        cy={arcY(segmentEnd.to)}
-        fill={`url(#${gradientShadowId})`}
-        mask={`url(#${maskEndId})`}
+        cx={cx}
+        cy={cy}
+        r={radius}
+        fill="none"
+        stroke={colorInterp(0.5)}
+        opacity={0.25}
+        strokeWidth={strokeWidth}
       />
 
       <rect
@@ -214,6 +224,26 @@ const AppleRing = ({
         fill={`url(#${gradientStartId})`}
         mask={`url(#${maskStartId})`}
       />
+
+      <circle
+        r={strokeWidth / 2 + 6}
+        cx={arcX(segmentEnd.to)}
+        cy={arcY(segmentEnd.to)}
+        fill={`url(#${gradientShadowId})`}
+        mask={`url(#${maskStartId})`}
+        opacity={angle > Math.PI * 1.5 ? 1 : 0}
+      />
+
+      {angle >= Math.PI && (
+        <rect
+          x={cx - gradientSide / 2}
+          y={cy - gradientSide / 2}
+          width={gradientSide}
+          height={gradientSide}
+          fill={`url(#${gradientEndId})`}
+          mask={`url(#${maskEndId})`}
+        />
+      )}
     </g>
   );
 };
